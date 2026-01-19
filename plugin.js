2ed6fe404c0f32bff41e6e61652a845aab7ec59a
NoobSaibot-0 <55320018+NoobSaibot-0@users.noreply.github.com>
2026-01-19 10:19:40 +1000

    Add files via upload

--- plugin.js
+++ plugin.js
@@ -1,59 +1,59 @@
 (function () {
     'use strict';
 
-    function LordPlugin() {
-        // Ссылка на ваш сервер в Termux
-        var network = new Lampa.Reguest();
-        var api_url = 'http://localhost:3000/';
+    function startLord() {
+        // Проверка в консоли (если есть возможность посмотреть)
+        console.log('Lordfilm: Plugin started');
+        
+        // Слушаем событие открытия карточки фильма
+        Lampa.Listener.follow('full', function (e) {
+            if (e.type == 'complite') {
+                console.log('Lordfilm: Full card opened', e.data);
 
-        this.start = function () {
-            Lampa.Listener.follow('full', function (e) {
-                if (e.type == 'complite') {
-                    var btn = $(`<div class="full-start__button selector">
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10 16.5L16 12L10 7.5V16.5ZM12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2Z" fill="white"/></svg>
-                        <span>Lordfilm</span>
-                    </div>`);
+                // Создаем кнопку. Используем классы Lampa для совместимости
+                var button = $(`<div class="full-start__button selector">
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10 16.5L16 12L10 7.5V16.5ZM12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2Z" fill="white"/>
+                    </svg>
+                    <span>Lordfilm</span>
+                </div>`);
 
-                    btn.on('hover:enter', function () {
-                        var title = e.data.movie.title || e.data.movie.name;
-                        Lampa.Noty.show('Поиск: ' + title);
-                        
-                        network.silent(api_url + 'search?q=' + encodeURIComponent(title), function (json) {
-                            if (json && json.length) {
-                                Lampa.Select.show({
-                                    title: 'Результаты Lordfilm',
-                                    items: json.map(function(i){ 
-                                        i.name = i.title; // Lampa требует поле name для списка
-                                        return i; 
-                                    }),
-                                    onSelect: function (item) {
-                                        Lampa.Noty.show('Выбрано: ' + item.title);
-                                        // Здесь будет логика запуска видео
-                                    },
-                                    onBack: function () {
-                                        Lampa.Controller.toggle('full_start');
-                                    }
-                                });
-                            } else {
-                                Lampa.Noty.show('Ничего не найдено');
-                            }
-                        }, function () {
-                            Lampa.Noty.show('Ошибка: Проверьте Termux (localhost:3000)');
-                        });
+                // Обработчик нажатия
+                button.on('hover:enter', function () {
+                    var title = e.data.movie.title || e.data.movie.name;
+                    Lampa.Noty.show('Ищу на Lordfilm: ' + title);
+                    
+                    // Проверка связи с Termux
+                    $.ajax({
+                        url: 'http://localhost:3000/search?q=' + encodeURIComponent(title),
+                        method: 'GET',
+                        success: function(data) {
+                            Lampa.Noty.show('Сервер ответил! Найдено: ' + data.length);
+                        },
+                        error: function() {
+                            Lampa.Noty.show('Ошибка: Проверьте Termux!');
+                        }
                     });
+                });
 
-                    e.object.container.find('.full-start__buttons').append(btn);
+                // Добавляем кнопку в контейнер. 
+                // Попробуем несколько вариантов селекторов на случай разных версий Lampa
+                var container = e.object.container.find('.full-start__buttons');
+                if (container.length) {
+                    container.append(button);
+                } else {
+                    // Запасной вариант добавления
+                    e.object.container.find('.full-start__controls').append(button);
                 }
-            });
-        };
+            }
+        });
     }
 
-    // Безопасный запуск
-    if (window.app_ready) {
-        new LordPlugin().start();
-    } else {
+    // Ожидание готовности приложения
+    if (window.app_ready) startLord();
+    else {
         Lampa.Listener.follow('app', function (e) {
-            if (e.type == 'ready') new LordPlugin().start();
+            if (e.type == 'ready') startLord();
         });
     }
 })();
